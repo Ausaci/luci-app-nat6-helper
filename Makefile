ffff903e4e292645f338d185357d4718ea83148c
Atomseek <Atomseek@outlook.com>
2022-11-01 17:45:05 +0800

    简化命令

--- Makefile
+++ Makefile
@@ -1,17 +1,14 @@
-
 include $(TOPDIR)/rules.mk
 
 PKG_NAME:=luci-app-nat6-helper
-PKG_VERSION:=v1.0
+PKG_VERSION:=v1.1
 PKG_RELEASE:=1
 
 PKG_LICENSE:=MIT License
 
 LUCI_TITLE:=LuCI support for nat6
-LUCI_DEPENDS:=+ip6tables +ip6tables-mod-nat +ip6tables-extra 
+LUCI_DEPENDS:=+ip6tables +kmod-ipt-nat6 
 LUCI_PKGARCH:=all
-
-PKG_MAINTAINER:=<https://github.com/Ausaci/luci-app-nat6-helper>
 
 include $(TOPDIR)/feeds/luci/luci.mk
 
--- luasrc/model/cbi/nat6-helper.lua
+++ luasrc/model/cbi/nat6-helper.lua
@@ -1,5 +1,5 @@
 m = Map("nat6-helper", "NAT6 配置助手") 
-m.description = translate("IPv6 路由器做 NAT6，使得路由器下级可以使用 IPv6 协议访问网站。<br />参考链接：<br />https://github.com/Ausaci/luci-app-nat6-helper<br />https://lixingcong.github.io/2017/04/24/ipv6-nat-lede/")
+m.description = translate("IPv6 路由器做 NAT6，使得路由器下级可以使用 IPv6 协议访问网站。<br />若插件启用失败，请检查路由器是否正常获取到IPv6")
 
 m:section(SimpleSection).template  = "nat6-helper/nat6_status"
 
@@ -14,25 +14,19 @@ enabled.rmempty = false
 name = s:option(Value, "name", translate("Interface"))
 name.rmempty = false
 name.default = "wan6"
-name.description = translate("填入 IPv6 接口名 (默认为 wan6 )，即可使 NAT6 随该接口状态自动开启或关闭")
-run = s:option(Button, "run_button", translate("启动"))
-run.inputstyle = "apply"
-function run.write(self, section)
-	io.popen("/etc/init.d/nat6-helper start")
-end
+name.description = translate("默认为wan6，也可自行设置为有ipv6的网口名称，当该网卡变动时自动设置nat6")
 
-stop = s:option(Button, "stop_button", translate("关闭"))
-stop.inputstyle = "apply"
-function stop.write(self, section)
-	io.popen("/etc/init.d/nat6-helper stop")
-end
-
-init = s:option(Button, "init_button", translate("初始化"))
-init.inputtitle = translate("执行 IPv6 初始化脚本")
+init = s:option(Button, "init_button", translate("一键设置"))
+init.inputtitle = translate("一键设置")
 init.inputstyle = "apply"
-init.description = translate("执行 IPv6 初始化脚本 ( /etc/ipv6nat.sh )，仅需执行一次！")
+init.description = translate("一键设置ULA和DHCPv6，开启nat6需进行的操作")
 function init.write(self, section)
-	io.popen("bash /etc/ipv6nat.sh >> /etc/ipv6nat.log 2>&1")
+	io.popen("/etc/init.d/nat6-helper setLan")
+end
+
+local apply = luci.http.formvalue("cbi.apply")
+if apply then
+    io.popen("/etc/init.d/nat6-helper restart")
 end
 
 return m
